dffb86d8220248269f35d8a7bf13259507e7c017
Terry Brown <ptbrown514@gmail.com>
2020-11-04 19:30:46 -0600

    second commit

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,15 +1,27 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import MaterialTable from 'material-table'
 
-function ItemList({ columns, data, title }) {
+function ItemList({ columns, data, title, showDetails }) {
     return(
         <MaterialTable
             columns={columns}
             data={data}
             title={title}
-        />
+            actions={[
+                {
+                    icon: () => <ViewIcon />,
+                    tooltip: "view Details",
+                    onClick: (_event, rowData) => showDetails(rowData)
+                }
+        ]} />
     )
 }
+
+const ViewIcon = styled.div`
+    background-color: #d2bba2;
+    height: 20px;
+    width: 50px;
+`
 
 export default ItemList
